ad6304fde010f57f626fb9b6abdb93e4d3866679
Millan Garcia <millangg17@gmail.com>
2020-11-19 10:30:30 +0100

    adding electron and sccs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "./utils/Firebase";
+import "firebase/auth";
+import { useState } from "react";
+import Auth from "./pages/Auth";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  firebase.auth().onAuthStateChanged((currentUser) => {
+    if (!currentUser) {
+      setUser(null);
+    } else {
+      setUser(currentUser);
+    }
+    setIsLoading(false);
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
+  return !user ? <Auth /> : <UserLogged />;
+
+  function UserLogged() {
+    const logOut = () => {
+      firebase.auth().signOut();
+    };
+    return (
+      <div>
+        style=
+        {{
+          display: "flex",
+          alignItem: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+        <h1> User Logged! </h1>
+        <button onClick={logOut}> Log Out</button>
+      </div>
+    );
+  }
 }
 
 export default App;
