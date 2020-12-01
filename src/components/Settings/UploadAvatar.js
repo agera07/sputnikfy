6a6ea5a8ade99a1f96289a251898ffd3be9370cb
Millan Garcia <millangg17@gmail.com>
2020-12-01 15:58:05 +0100

    updating avatar drop in photo

--- src/components/Settings/UploadAvatar.js
+++ src/components/Settings/UploadAvatar.js
@@ -1,14 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import { Image } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
+import { toast } from "react-toastify";
 import NoAvatar from "../../assests/png/user.png";
+import firebase from "../../utils/Firebase";
+import "firebase/storage";
+import "firebase/auth";
 
 export default function UploadAvatar(props) {
   const { user } = props;
 
   const [avatarUrl, setAvatarUrl] = useState(user.photoURL);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    setAvatarUrl(URL.createObjectURL(file));
+    uploadImage(file).then(() => {
+      updateUserAvatar();
+    });
+  });
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: "image/png, image/jpeg",
+    noKeyboard: true,
+    onDrop,
+  });
+
+  const uploadImage = (file) => {
+    const ref = firebase.storage().ref().child(`avatar/${user.uid}`);
+    return ref.put(file);
+  };
+
+  const updateUserAvatar = () => {
+    firebase
+      .storage()
+      .ref(`avatar/${user.uid}`)
+      .getDownloadURL()
+      .then((response) => {
+        firebase.auth().currentUser.updateProfile({ photoURL: response });
+      })
+      .catch(() => {
+        toast.error("Error to upload image!");
+      });
+  };
+
   return (
-    <div className="user-avatar">
-      <Image src={avatarUrl ? avatarUrl : NoAvatar} />
+    <div className="user-avatar" {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <Image src={NoAvatar} />
+      ) : (
+        <Image src={avatarUrl ? avatarUrl : NoAvatar} />
+      )}
     </div>
   );
 }
