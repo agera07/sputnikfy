8ed9f6175e90ee208b39f0787895b1880276cdff
Millan Garcia <millangg17@gmail.com>
2020-12-07 18:40:26 +0100

    adding slick carousel and lodash libs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BannerHome from "../../components/BannerHome";
+import firebase from "../../utils/Firebase";
+import "firebase/firestore";
+import { map } from "lodash";
+import BasicSliderItems from "../../components/Sliders/BasicSliderItems";
+
 import "./Home.scss";
 
+const db = firebase.firestore(firebase);
+
 export default function Home() {
+  const [artists, setAstists] = useState([]);
+
+  useEffect(() => {
+    db.collection("artists")
+      .get()
+      .then((response) => {
+        const arrayArtists = [];
+        map(response?.docs, (artist) => {
+          const data = artist.data();
+          data.id = artist.id;
+          arrayArtists.push(data);
+        });
+        setAstists(arrayArtists);
+      });
+  }, []);
+
   return (
     <>
       <BannerHome />
       <div className="home">
+        <BasicSliderItems title="Last Artists" data={artists} />
         <h2>hello world</h2>
       </div>
     </>
